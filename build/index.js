626b15d147944f4a7e12b4b14852f06e02913c84
puskar kafle <75001022+puskarkafle12@users.noreply.github.com>
2023-01-04 11:14:29 +0545

    logged in sucess

--- build/index.js
+++ build/index.js
@@ -30,21 +30,82 @@ app.post('/addstudent', (req, res) => {
         }
     });
 });
+app.get('/students', function (req, res) {
+    client.query('SELECT * FROM student', function (err, result) {
+        if (err) {
+            res.send(err + "errroeoreor");
+        }
+        else {
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+});
+app.get('/students/:id', (req, res) => {
+    // const id=request.params.id
+    const id = req.params.id;
+    client.query('SELECT * FROM student WHERE studentid = $1', [id], (err, result) => {
+        if (err) {
+            res.json({ "error": "bad request",
+                "error desc": err });
+        }
+        else {
+            res.json(result.rows);
+        }
+    });
+});
+app.get('/subjects', (req, res) => {
+    // const id=request.params.id
+    const id = req.params.id;
+    client.query('SELECT * FROM subject', (err, result) => {
+        if (err) {
+            res.json({ "error": "bad request",
+                "error desc": err });
+        }
+        else {
+            res.json(result.rows);
+        }
+    });
+});
+app.get('/subject/:id', (req, res) => {
+    // const id=request.params.id
+    const id = req.params.id;
+    client.query('SELECT * FROM subject WHERE id = $1', [id], (err, result) => {
+        if (err) {
+            res.json({ "error": "bad request",
+                "error desc": err });
+        }
+        else {
+            res.json(result.rows);
+        }
+    });
+});
+app.post('/addsubject', (req, res) => {
+    const { studentid, name } = req.body;
+    const query = `
+	  INSERT INTO subject (studentid,name)
+	  VALUES ($1, $2)
+	`;
+    client.query(query, [studentid, name], (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.json({ "message": "subject added sucessfully" });
+        }
+    });
+});
+app.delete('/student:id', (req, res) => {
+    const id = req.params.id;
+    client.query('DELETE FROM student WHERE studentid =$1;', [id], (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log(`Deleted student with id ${id}`);
+        }
+    });
+});
 app.listen(3000, function () {
     console.log('Server is running at https://localhost:3000');
 });
-app.get("/students", (req, res) => {
-    try {
-        var result = client.query('SELECT * FROM student');
-        const jsonResult = result.toJSON();
-        res.json(jsonResult);
-    }
-    catch (error) {
-        console.log(error);
-        res.sendStatus(400);
-        res.json({ "error": "bad request" });
-    }
-    finally {
-        client.end();
-    }
-});
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { json } from 'body-parser';
 import { Request, Response } from 'express';
+import { request } from 'http';
+import { setUncaughtExceptionCaptureCallback } from 'process';
 
 
 var express = require('express');
@@ -32,9 +34,7 @@ app.post('/addstudent', (req: Request, res: Response) => {
 	  }
 	});
   });
-  app.listen(3000, function () {
-    console.log('Server is running at https://localhost:3000');
-});
+  
 
 app.get('/students', function (req:Request, res:Response) {
 	client.query('SELECT * FROM student', function (err:Error, result:any) {
@@ -46,5 +46,107 @@ app.get('/students', function (req:Request, res:Response) {
 	  });
 	  
 	  
+	
 	  
+	  
+});
+app.get('/students/:id',  (req:Request,res:Response)=>{
+	// const id=request.params.id
+	const id = req.params.id
+	 client.query('SELECT * FROM student WHERE id = $1', [id],(err:Error,result:any)=>{
+		if(err){
+			res.json({"error":"bad request",
+		"error desc":err});
+		}
+		else{
+			res.json(result.rows)
+		}
+
+	 });
+
+});
+
+app.get('/subjects',  (req:Request,res:Response)=>{
+	// const id=request.params.id
+	const id = req.params.id
+	 client.query('SELECT * FROM subject',(err:Error,result:any)=>{
+		if(err){
+			res.json({"error":"bad request",
+		"error desc":err});
+		}
+		else{
+			res.json(result.rows)
+		}
+
+	 });
+
+});
+app.get('/subject/:id',  (req:Request,res:Response)=>{
+	// const id=request.params.id
+	const id = req.params.id
+	 client.query('SELECT * FROM subject WHERE id = $1', [id],(err:Error,result:any)=>{
+		if(err){
+			res.json({"error":"bad request",
+		"error desc":err});
+		}
+		else{
+			res.json(result.rows)
+		}
+
+	 });
+
+});
+
+app.post('/addsubject', (req: Request, res: Response) => {
+	const { id, name } = req.body;
+	const query = `
+	  INSERT INTO subject (id,name)
+	  VALUES ($1, $2)
+	`;
+	client.query(query, [id,name], (err:any, result:any) => {
+	  if (err) {
+		res.send(err);
+	  } else {
+		res.json({"message":"subject added sucessfully"});
+	  }
+	});
+  });
+app.delete('/student:id',(req:Request,res:Response)=>{
+	const id = req.params.id;
+	client.query('DELETE FROM student WHERE id =$1;', [id], (error:Error, result:any) => {
+	  if (error) {
+		console.log(error);
+	  } else {
+		console.log(`Deleted student with id ${id}`);
+	  }
+	});
+})
+  
+app.post('/login', (req: Request, res: Response) => {
+	// get the username and password from the request body
+	const { email, password } = req.body;
+  
+	// check if the username and password are correct
+	client.query('SELECT * FROM student WHERE email = $1 AND password = $2', [email, password], (err:Error, result:any) => {
+	  if (err) {
+		// there was an error executing the query
+		res.status(500).send(err);
+	  } else if (result.rowCount === 0) {
+		// the username or password was incorrect
+		res.status(401).send({ message: 'Username or password is incorrect' });
+	  } else {
+		// the login was successful
+		res.send({ message: 'Successfully logged in' });
+	  }
+	});
+  });
+  
+  
+  
+  
+  
+
+
+  app.listen(3000, function () {
+    console.log('Server is running at https://localhost:3000');
 });
